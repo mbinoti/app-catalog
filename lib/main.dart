62a7574639cc9ef43b3bd830aa89f162c8c89574
Marcos <mbinoti@gmail.com>
2023-05-18 12:04:55 -0300

    init

--- lib/main.dart
+++ lib/main.dart
@@ -1,124 +1,202 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a blue toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: LoginPage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
+class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+            const TextField(
+              // criar a validacao do e-mail e senha
+
+              decoration: InputDecoration(hintText: 'Email'),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+            const TextField(
+              decoration: InputDecoration(hintText: 'Senha'),
+              obscureText: true,
+            ),
+            ElevatedButton(
+              child: const Text('Entrar'),
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => CatalogPage()),
+                );
+              },
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
+
+class CatalogItem {
+  final String name;
+  final String color;
+  final double value;
+
+  CatalogItem({required this.name, required this.color, required this.value});
+}
+
+class CatalogPage extends StatefulWidget {
+  @override
+  _CatalogPageState createState() => _CatalogPageState();
+}
+
+class _CatalogPageState extends State<CatalogPage> {
+  final cart = ValueNotifier<List<CatalogItem>>([]);
+  final catalogItems = [
+    CatalogItem(name: 'Item 1', color: 'Azul', value: 10),
+    CatalogItem(name: 'Item 2', color: 'Vermelho', value: 20),
+    CatalogItem(name: 'Item 3', color: 'Verde', value: 30),
+    // adicione mais itens conforme necessário
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Catálogo'),
+        actions: [
+          ValueListenableBuilder(
+            valueListenable: cart,
+            builder: (context, List<CatalogItem> value, _) {
+              return IconButton(
+                icon: Stack(
+                  children: <Widget>[
+                    const Icon(Icons.shopping_cart),
+                    Positioned(
+                      right: 0,
+                      child: Container(
+                        padding: const EdgeInsets.all(1),
+                        decoration: BoxDecoration(
+                          color: Colors.red,
+                          borderRadius: BorderRadius.circular(6),
+                        ),
+                        constraints: const BoxConstraints(
+                          minWidth: 12,
+                          minHeight: 12,
+                        ),
+                        child: Text(
+                          '${value.length}',
+                          style: const TextStyle(
+                            color: Colors.white,
+                            fontSize: 8,
+                          ),
+                          textAlign: TextAlign.center,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => CartPage(cart)),
+                  );
+                },
+              );
+            },
+          ),
+        ],
+      ),
+      body: ListView.builder(
+        itemCount: catalogItems.length,
+        itemBuilder: (context, index) {
+          final item = catalogItems[index];
+          return ListTile(
+            leading: Container(
+              width: 24,
+              height: 24,
+              color: colorFromString(item.color),
+            ),
+            title: Text(item.name),
+            // subtitle: Text('Cor: ${item.color}'),
+            trailing: IconButton(
+              icon: const Icon(Icons.check),
+              onPressed: () => cart.value = [...cart.value, item],
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
+
+Color colorFromString(String colorName) {
+  switch (colorName) {
+    case 'Azul':
+      return Colors.blue;
+    case 'Vermelho':
+      return Colors.red;
+    case 'Verde':
+      return Colors.green;
+    // adicione mais cores conforme necessário
+    default:
+      return Colors.black; // cor padrão caso o nome da cor não seja reconhecido
+  }
+}
+
+class CartPage extends StatelessWidget {
+  final ValueNotifier<List<CatalogItem>> cart;
+
+  CartPage(this.cart);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Carrinho'),
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back),
+          onPressed: () => Navigator.pop(context),
+        ),
+      ),
+      body: ValueListenableBuilder(
+        valueListenable: cart,
+        builder: (context, List<CatalogItem> value, _) {
+          return ListView.builder(
+            itemCount: value.length,
+            itemBuilder: (context, index) {
+              return ListTile(
+                title: Text(value[index].name),
+                subtitle: Text('Cor: ${value[index].color}'),
+                trailing: IconButton(
+                  icon: const Icon(Icons.remove_shopping_cart),
+                  onPressed: () {
+                    cart.value = List.from(cart.value)..removeAt(index);
+                  },
+                ),
+              );
+            },
+          );
+        },
+      ),
+      bottomNavigationBar: ValueListenableBuilder(
+        valueListenable: cart,
+        builder: (context, List<CatalogItem> value, _) {
+          final totalValue = value.fold<double>(
+              0, (previousValue, item) => previousValue + item.value);
+          return Text(
+              'Total de itens no carrinho: ${value.length}, Valor total: \$${totalValue.toStringAsFixed(2)}');
+        },
+      ),
     );
   }
 }
